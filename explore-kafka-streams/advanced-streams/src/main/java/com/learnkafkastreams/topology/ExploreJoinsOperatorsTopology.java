d4a98bd96edc07dc60dd77578923d0e73decd9db
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-12-24 16:33:43 +0100

    Explore innerJoin using join operator - Joining KStream and KTable

--- explore-kafka-streams/advanced-streams/src/main/java/com/learnkafkastreams/topology/ExploreJoinsOperatorsTopology.java
+++ explore-kafka-streams/advanced-streams/src/main/java/com/learnkafkastreams/topology/ExploreJoinsOperatorsTopology.java
@@ -17,10 +17,35 @@ public class ExploreJoinsOperatorsTopology {
     public static String ALPHABETS_ABBREVATIONS = "alphabets_abbreviations"; // A=> Apple
 
 
-    public static Topology build(){
+    public static Topology build() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
 
+        joinKStreamWithKTable(streamsBuilder);
+
         return streamsBuilder.build();
+    }
+
+    private static void joinKStreamWithKTable(StreamsBuilder streamsBuilder) {
+        var alphabetAbbreviation = streamsBuilder
+                .stream(ALPHABETS_ABBREVATIONS, Consumed.with(Serdes.String(), Serdes.String()));
+
+        alphabetAbbreviation
+                .print(Printed.<String, String>toSysOut().withLabel(ALPHABETS_ABBREVATIONS));
+
+        var alphabetsTable = streamsBuilder
+                .table(ALPHABETS, Consumed.with(Serdes.String(), Serdes.String()), Materialized.as("alphabets-store"));
+
+        alphabetsTable
+                .toStream()
+                .print(Printed.<String, String>toSysOut().withLabel(ALPHABETS));
+
+        ValueJoiner<String, String, Alphabet> valueJoiner = Alphabet::new;
+
+        var joinedStream = alphabetAbbreviation
+                .join(alphabetsTable, valueJoiner);
+
+        joinedStream
+                .print(Printed.<String, Alphabet>toSysOut().withLabel("alphabets-with-abbreviation"));
     }
 
 }
